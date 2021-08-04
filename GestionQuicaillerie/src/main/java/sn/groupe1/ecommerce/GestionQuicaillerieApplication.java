5488f61f0423fcafa62efcc8838a0e7c1cc68fce
OmzoPro <ndiayeomarthiate@gmail.com>
2021-08-04 20:14:05 +0000

    Ajout du projet inital dans github, premier commit

---
+++ GestionQuicaillerie/src/main/java/sn/groupe1/ecommerce/GestionQuicaillerieApplication.java
@@ -0,0 +1,13 @@
+package sn.groupe1.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionQuicaillerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionQuicaillerieApplication.class, args);
+	}
+
+}
---
+++ GestionQuicaillerie/src/test/java/sn/groupe1/ecommerce/GestionQuicaillerieApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.groupe1.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionQuicaillerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
